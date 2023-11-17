864ba1e4e5c3b1d751c561b185a5f565862c37dc
TurtleWake <90726868+TurtleWake@users.noreply.github.com>
2023-11-17 13:28:08 -0500

    finished all basic logic.

--- whac-a-mole/app.js
+++ whac-a-mole/app.js
@@ -1,1 +1,48 @@
 const squares = document.querySelectorAll('.square')
+const mole = document.querySelector('.mole')
+const timeLeft = document.querySelector('#time-left')
+const score = document.querySelector('#score')
+
+let result = 0
+let hitPosistion
+let currentTime = 60
+
+function randomSquare() {
+    squares.forEach(square => {
+        square.classList.remove('mole')
+    })
+
+    let randomSquare = squares[Math.floor(Math.random() * 9)]
+    randomSquare.classList.add('mole')
+
+    hitPosistion = randomSquare.id
+}
+
+squares.forEach(square => {
+    square.addEventListener('mousedown', () => {
+        if (square.id == hitPosistion) {
+            result++
+            score.textContent = result
+            hitPosistion = null
+        }
+    })
+})
+
+function moveMole() {
+    let timerId = null 
+    timerId = setInterval(randomSquare, 500)
+}
+
+moveMole()
+
+function countDown() {
+    currentTime--
+    timeLeft.textContent = currentTime
+
+    if (currentTime == 0) {
+        clearInterval(countDownTimerId)
+        alert('GAME OVER! Your score is' + ' ' + result)
+    }
+}
+
+let countDownTimerId = setInterval(countDown, 500)
